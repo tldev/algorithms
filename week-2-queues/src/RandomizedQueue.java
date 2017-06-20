4c867193bd5fa09e827b2b2ebe75f48264f88b8c
Tom Johnell <tjohnell@gmail.com>
2017-06-19 22:56:06 -0400

    Implement queue backed by array w/ balancing

--- week-2-queues/src/RandomizedQueue.java
+++ week-2-queues/src/RandomizedQueue.java
@@ -1,12 +1,23 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
 
 /**
  * Created by tjohnell on 6/18/17.
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private static final int INITIAL_ARRAY_SIZE = 10;
+
+    private int head;
+    private int tail;
+    private int size;
+    private Item[] queue;
+
     // construct an empty randomized queue
     public RandomizedQueue() {
-
+        queue = createArray(INITIAL_ARRAY_SIZE);
+        tail = 0;
+        size = 0;
     }
 
     // is the queue empty?
@@ -16,17 +27,39 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // return the number of items on the queue
     public int size()                         {
-        return 0;
+        return size;
     }
 
     // add the item
     public void enqueue(Item item) {
+        if (tail == queue.length) {
+            tail = 0;
+        }
 
+        queue[tail++] = item;
+        ++size;
+
+        // We need to up the capacity
+        if (size > queue.length / 2.0) {
+            changeQueueSize(queue.length * 2);
+        }
     }
 
     // remove and return a random item
     public Item dequeue() {
-        return null;
+        Item item = queue[head];
+        queue[head++] = null;
+        --size;
+
+        if (head == queue.length) {
+            head = 0;
+        }
+
+        if (size < queue.length / 4.0) {
+            changeQueueSize(queue.length / 2);
+        }
+
+        return item;
     }
 
     // return (but do not remove) a random item
@@ -52,6 +85,31 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
             }
         };
+    }
+
+    private void changeQueueSize(int newSize) {
+        Item[] newQueue = createArray(newSize);
+        int j = 0;
+        if (head > tail) {
+            for(int i = head; i < queue.length; i++) {
+                newQueue[j++] = queue[i];
+            }
+            for(int i = 0; i < tail; i++) {
+                newQueue[j++] = queue[i];
+            }
+        } else {
+            for(int i = head; i < tail; i++) {
+                newQueue[j++] = queue[i];
+            }
+        }
+
+        queue = newQueue;
+        head = 0;
+        tail = j;
+    }
+
+    private Item[] createArray(int size) {
+        return (Item[])new Object[size];
     }
 
     public static void main(String[] args) {
