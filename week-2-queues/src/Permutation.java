6eca227bbd5e8f679554f8f4fa1db7602510899f
Tom Johnell <tjohnell@gmail.com>
2017-06-21 10:30:59 -0400

    Implement perumation with randomized queue

--- week-2-queues/src/Permutation.java
+++ week-2-queues/src/Permutation.java
@@ -1,8 +1,20 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * Created by tjohnell on 6/18/17.
  */
 public class Permutation {
     public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
 
+        for (int i = 0; i < k; i++) {
+            queue.enqueue(StdIn.readString());
+        }
+
+        for (String s : queue) {
+            StdOut.println(s);
+        }
     }
 }
