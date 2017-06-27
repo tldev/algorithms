d0ea8b87d06c7e2810f4509483ec7e0d8fa52762
Tom Johnell <tjohnell@gmail.com>
2017-06-26 20:56:39 -0400

    Implements naive brute foce pattern recognition

--- week-3-pattern-recognition/src/BruteCollinearPoints.java
+++ week-3-pattern-recognition/src/BruteCollinearPoints.java
@@ -1,17 +1,44 @@
+import java.util.Arrays;
+import java.util.Stack;
+
 public class BruteCollinearPoints {
+
+    private static final int MAX_MATCHES = 4;
+    private LineSegment[] segments;
+    private Point[] points;
 
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
+        Stack<LineSegment> lineSegments = new Stack<LineSegment>();
+        Arrays.sort(points);
 
+        for(int i = 0; i < points.length - 3; i ++) {
+            for (int j = i + 1; j < points.length - 2; j++) {
+                double ij = points[i].slopeTo(points[j]);
+                for (int k = j + 1; k < points.length - 1; k++) {
+                    double ik = points[i].slopeTo(points[k]);
+                    if (ik == ij) {
+                        for (int l = k + 1; l < points.length; l++) {
+                            double il = points[i].slopeTo(points[l]);
+                            if (il == ij) {
+                                lineSegments.push(new LineSegment(points[i], points[j]));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        segments = (LineSegment[]) lineSegments.toArray();
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        return 0;
+        return segments.length;
     }
 
     // the line segments
     public LineSegment[] segments() {
-        return new LineSegment[0];
+        return segments;
     }
 }
