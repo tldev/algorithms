a04b3bf13547bba917ba93dd77ec3048b0f3a29a
Tom Johnell <tjohnell@gmail.com>
2017-07-01 13:45:16 -0400

    Different strategy, for efficient

--- week-3-pattern-recognition/src/BruteCollinearPoints.java
+++ week-3-pattern-recognition/src/BruteCollinearPoints.java
@@ -1,6 +1,4 @@
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 public class BruteCollinearPoints {
@@ -12,17 +10,33 @@ public class BruteCollinearPoints {
         if (points == null) {
             throw new java.lang.IllegalArgumentException();
         }
-        points = deDupe(points);
+
+        for (Point p : points) {
+            if (p == null) {
+                throw new java.lang.IllegalArgumentException();
+            }
+        }
+
+        points = points.clone();
         Stack<LineSegment> lineSegments = new Stack<>();
         Arrays.sort(points);
 
         for (int i = 0; i < points.length - 3; i++) {
             for (int j = i + 1; j < points.length - 2; j++) {
+                if (points[i].compareTo(points[j]) == 0) {
+                    continue;
+                }
                 double ij = points[i].slopeTo(points[j]);
                 for (int k = j + 1; k < points.length - 1; k++) {
+                    if(points[j].compareTo(points[k]) == 0) {
+                        continue;
+                    }
                     double ik = points[i].slopeTo(points[k]);
                     if (isEqualSlopes(ik, ij)) {
                         for (int l = k + 1; l < points.length; l++) {
+                            if(points[k].compareTo(points[l]) == 0) {
+                                continue;
+                            }
                             double il = points[i].slopeTo(points[l]);
                             if (isEqualSlopes(il, ij)) {
                                 lineSegments.push(new LineSegment(points[i], points[l]));
@@ -52,26 +66,5 @@ public class BruteCollinearPoints {
 
     private boolean isEqualSlopes(double slope1, double slope2) {
         return slope1 == slope2 || Math.abs(slope1 - slope2) < 0.000001;
-    }
-
-    private Point[] deDupe(Point[] points) {
-        HashMap<String, Point> map = new HashMap<>(points.length);
-        for (Point p : points) {
-            if (p == null) {
-                throw new java.lang.IllegalArgumentException();
-            }
-
-            if (!map.containsKey(p.toString())) {
-                map.put(p.toString(), p);
-            }
-        }
-
-        Point[] pointSet = new Point[map.size()];
-        int i = 0;
-        for (Map.Entry<String, Point> e : map.entrySet()) {
-            pointSet[i] = e.getValue();
-        }
-
-        return pointSet;
     }
 }
