58f35706fffceda2b340d41c412817b37a5e2812
Tom Johnell <tjohnell@gmail.com>
2017-07-16 15:57:43 -0400

    Edge case exceptions

--- week-5-kd-trees/src/PointSET.java
+++ week-5-kd-trees/src/PointSET.java
@@ -40,11 +40,19 @@ public class PointSET {
 
     // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
         return new Stack<Point2D>();
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
+        if (p == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
         return new Point2D(0.0, 0.0);
     }
 
