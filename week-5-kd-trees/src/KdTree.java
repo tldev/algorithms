489307155743433bdc227fd1eb961b3cdeb20773
Tom Johnell <tjohnell@gmail.com>
2017-07-17 22:07:44 -0400

    First attempt at real KD Tree

--- week-5-kd-trees/src/KdTree.java
+++ week-5-kd-trees/src/KdTree.java
@@ -4,42 +4,38 @@ import edu.princeton.cs.algs4.*;
  * Created by tjohnell on 7/16/17.
  */
 public class KdTree {
-    private SET<Point2D> set;
+    private Node root;
+    private int size;
 
     // construct an empty set of points
     public KdTree() {
-        set = new SET<Point2D>();
-        Node node = new Node();
-    }
-
-    private class Node {
-
+        size = 0;
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return set.isEmpty();
+        return size == 0;
     }
 
     // number of points in the set
     public int size() {
-        return set.size();
+        return size;
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
-        set.add(p);
+        root = put(root, p, 0);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return set.contains(p);
+        return _contains(root, p, 0);
     }
 
     // draw all points to standard draw
     public void draw() {
         StdDraw.rectangle(0.5, 0.5, 0.5, 0.5);
-        for(Point2D point : set) {
+        for (Point2D point : points()) {
             StdDraw.point(point.x(), point.y());
         }
     }
@@ -50,15 +46,61 @@ public class KdTree {
             throw new java.lang.IllegalArgumentException();
         }
 
-        Stack<Point2D> points = new Stack<Point2D>();
-        for (Point2D point : set) {
-            if (rect.contains(point)) {
-                points.push(point);
-            }
-        }
+        Queue<Point2D> points = new Queue<Point2D>();
+        rangeAndEnqueue(root, rect, 0, points);
 
         return points;
     }
+
+    private Iterable<Point2D> points() {
+        Queue<Point2D> points = new Queue<Point2D>();
+        inorder(root, points);
+
+        return points;
+    }
+
+    private void inorder(Node x, Queue<Point2D> q) {
+        if (x == null) return;
+        inorder(x.left, q);
+        q.enqueue(x.point);
+        inorder(x.right, q);
+    }
+
+    private void rangeAndEnqueue(Node h, RectHV rect, int level, Queue<Point2D> q) {
+        if (h == null) return;
+
+
+        if (level % 2 == 0) {
+            if (Double.compare(h.point.x(), rect.xmin()) <= 0) {
+                if (rect.contains(h.point)) {
+                    q.enqueue(h.point);
+                }
+                rangeAndEnqueue(h.right, rect, level + 1, q);
+            }
+
+            if(Double.compare(h.point.x(), rect.xmax()) >= 0) {
+                if (rect.contains(h.point)) {
+                    q.enqueue(h.point);
+                }
+                rangeAndEnqueue(h.left, rect, level + 1, q);
+            }
+        } else {
+            if (Double.compare(h.point.y(), rect.ymin()) <= 0) {
+                if (rect.contains(h.point)) {
+                    q.enqueue(h.point);
+                }
+                rangeAndEnqueue(h.right, rect, level + 1, q);
+            }
+
+            if (Double.compare(h.point.y(), rect.ymax()) >= 0) {
+                if (rect.contains(h.point)) {
+                    q.enqueue(h.point);
+                }
+                rangeAndEnqueue(h.right, rect, level + 1, q);
+            }
+        }
+    }
+
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
@@ -66,16 +108,42 @@ public class KdTree {
             throw new java.lang.IllegalArgumentException();
         }
 
-        Point2D nearest = null;
-        double smallestDistance = Double.POSITIVE_INFINITY;
-        for (Point2D point : set) {
-            double curDistance = p.distanceTo(point);
-            if (nearest == null || curDistance < smallestDistance) {
-                nearest = point;
-                smallestDistance = curDistance;
-            }
-        }
-
-        return nearest;
+        return root.point;
     }
+
+
+    private class Node {
+        Point2D point;
+        int level;
+        Node left, right;
+
+        Node(Point2D point, int level) {
+            this.level = level;
+            this.point = point;
+        }
+    }
+
+    private Node put(Node h, Point2D point, int level) {
+        if (h == null)  {
+            size++;
+            return new Node(point, level);
+        }
+        int cmp = level % 2 == 0 ? Double.compare(h.point.x(), point.x()) : Double.compare(h.point.y(), point.y());
+
+        if (cmp < 0) h.left = put(h.left, point, level + 1);
+        else if (h.point.compareTo(point) != 0) h.right = put(h.right, point, level + 1);
+
+        return h;
+    }
+
+    private boolean _contains(Node h, Point2D point, int level) {
+        if (point == null) return false;
+        int cmp = level % 2 == 0 ? Double.compare(h.point.x(), point.x()) : Double.compare(h.point.y(), point.y());
+
+        if (cmp < 0)
+            return _contains(h.left, point, level + 1);
+        else
+            return h.point.compareTo(point) == 0 || _contains(h.right, point, level + 1);
+    }
+}
 }
